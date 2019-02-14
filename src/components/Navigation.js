2ebd02adbe1cf24d77860255a99c096959e43c18
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-02-14 12:30:20 +0200

    fix indent

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
-    <nav>
-        <NavLink to='/home' activeStyle={{color: 'green'}}>Home</NavLink>
-        <NavLink to='/choose' activeStyle={{color: 'green'}}>Choose</NavLink>
-        {/* <NavLink to='/ticket' activeStyle={{color: 'green'}}>All Items</NavLink> */}
-    </nav>
+  <nav>
+    <NavLink to='/home' activeStyle={{ color: 'green' }}>Home</NavLink>
+    <NavLink to='/choose' activeStyle={{ color: 'green' }}>Choose</NavLink>
+    {/* <NavLink to='/ticket' activeStyle={{color: 'green'}}>All Items</NavLink> */}
+  </nav>
 );
 
 export default Navigation;
