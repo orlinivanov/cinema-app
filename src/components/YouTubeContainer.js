ea46a35bf7c25cda578ed1f2e7ef95f47350c868
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-02-14 16:10:37 +0200

    add youtube container component

---
+++ src/components/YouTubeContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const YouTubeContainer = (props) => {
+  return (
+    <iframe 
+      width="480" 
+      height="270" 
+      src={`https://www.youtube.com/embed/${props.trailerYouTubeId}`} 
+      title={props.title} 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+      allowFullScreen>
+    </iframe>
+  );
+}
+
+export default YouTubeContainer;
