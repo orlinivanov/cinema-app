d2fe56f9c14ed40bed52ddfdb75dc8fca31ba10e
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-02-19 15:59:13 +0200

    change logo file

--- src/components/YouTubeLink.js
+++ src/components/YouTubeLink.js
@@ -3,7 +3,7 @@ import React from 'react';
 const YouTubeLink = (props) => {
   return (
     <a href={`https://www.youtube.com/watch?v=${props.videoId}`} target="_blank" rel="noopener noreferrer">
-      <img src="yt_logo_rgb_light_80x18.png" alt="Open in YouTube" />
+      <img src="yt_logo_rgb_dark.png" alt="Open in YouTube" width="80" height="18" />
     </a>
   );
 }
