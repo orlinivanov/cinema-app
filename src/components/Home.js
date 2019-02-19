d3702d76a360cb684cd43ec38fef0be17c5b699d
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-02-19 16:02:41 +0200

    fix some elements and remove some consoloe.logs

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,23 +4,25 @@ import IMDBRating from './IMDBRating';
 import YouTubeLink from './YouTubeLink';
 
 const Home = (props) => {
-  console.log(props);
+  // console.log(props);
   if (props.selectedMovie.imdbId) {
     return <Redirect to='/choose' />
   }
   return (
-    <div className="movies-container">
-      {/* <h4>Choose your movie:</h4> */}
-      {props.movies.map((movie, i) => (
-        <div key={i} className='pick-movie-container'>
-          <div data-imdbid={movie.imdbId} onClick={props.selectMovie}>{movie.title}</div>
-          <div>
-            <IMDBRating titleId={movie.imdbId} />
-            <YouTubeLink videoId={movie.trailerYouTubeId} />
+    <>
+      <h2>Now showing:</h2>
+      <div className="movies-container">
+        {props.movies.map((movie, i) => (
+          <div key={i} className='pick-movie-container'>
+            <div className='movie-title' data-imdbid={movie.imdbId} onClick={props.selectMovie}>{movie.title}</div>
+            <div className='movie-details'>
+              <IMDBRating titleId={movie.imdbId} />
+              <YouTubeLink videoId={movie.trailerYouTubeId} />
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
 
