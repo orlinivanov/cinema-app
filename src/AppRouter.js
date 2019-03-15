0857327846449fc4e142dedfd5dcbcd1ea882e47
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-03-15 17:54:17 +0200

    add default redirect

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import Choose from './components/ChooseSeats';
 import Tickets from './components/Tickets';
 
-const AppRouter = (props) => {
-  const parentProps = props;
+const AppRouter = (parentProps) => {
+  // const parentProps = props;
   return (
     <Switch>
-      {/* <Route path='/' render={(props) => <Home {...props} {...parentProps} />} /> */}
+      <Route exact path='/' render={(props) => <Home {...props} {...parentProps} />} />
       <Route path='/home' render={(props) => <Home {...props} {...parentProps} />} />
       {/* <Route path='/home' component={Home} /> */}
       <Route path='/choose' render={(props) => <Choose {...props} {...parentProps} />} />
       <Route path='/tickets' render={(props) => <Tickets {...props} {...parentProps} />} />
+      <Redirect to='/home' />
     </Switch>
   )
 };
