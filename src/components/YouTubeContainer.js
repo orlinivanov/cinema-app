48e1c5d8de1386216bf7a0cff5a641b52b32b4eb
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-04-15 12:55:27 +0300

    add loading message to trailer container

--- src/components/YouTubeContainer.js
+++ src/components/YouTubeContainer.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const YouTubeContainer = (props) => {
   return (
-    <iframe 
-      width="480" 
-      height="270" 
-      src={`https://www.youtube.com/embed/${props.trailerYouTubeId}`} 
-      title={props.title} 
-      frameBorder="0" 
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-      allowFullScreen>
-    </iframe>
+    <div className='trailer-container'>
+      Trailer loading...
+        <iframe
+          width="480"
+          height="270"
+          src={`https://www.youtube.com/embed/${props.trailerYouTubeId}`}
+          title={props.title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen>
+        </iframe>
+    </div >
+
   );
 }
 
