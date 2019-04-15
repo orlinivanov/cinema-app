4a8cf838b30e10f3251d9b905b044cd6a889d4c6
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-04-15 12:57:09 +0300

    add navigation and fix formatting

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Navigation from './Navigation';
 
 const Tickets = (props) => {
   if (!props.selectedMovie.imdbId) {
@@ -15,31 +16,32 @@ const Tickets = (props) => {
   tomorrow.setSeconds(0);
   return (
     <>
-    <h2>Your tickets:</h2>
-    <button 
-      onClick={() => {
-        props.setNewState(prevState => ({
-          selectedMovie: {},
-          numberOfTickets: 0,
-          selectedSeats: [],
-          reservationConfirmed: false
-        }));
-      }}
-    >
-      Start Again
+      <Navigation />
+      <h2>Your tickets:</h2>
+      <button
+        onClick={() => {
+          props.setNewState(prevState => ({
+            selectedMovie: {},
+            numberOfTickets: 0,
+            selectedSeats: [],
+            reservationConfirmed: false
+          }));
+        }}
+      >
+        Start Again
     </button>
-    <div className='tickets-list'>
-      {props.selectedSeats.map((seat, i) => (
+      <div className='tickets-list'>
+        {props.selectedSeats.map((seat, i) => (
           <div key={i} className='ticket'>
             <h3>Zelena Polyana Theatre</h3>
             <h4>{props.selectedMovie.title}</h4>
             <p>Row: {seat.row} Seat: {seat.seat}</p>
             <p>{tomorrow.toLocaleString('en-GB')}</p>
-            <img src="qr-code.png" alt="QR Code" width="150" height="150"/>
+            <img src="qr-code.png" alt="QR Code" width="150" height="150" />
           </div>
         )
-      )}
-    </div>
+        )}
+      </div>
     </>
   );
 };
