815d022d963b7b6a8aacccf0479bc8b53f48061b
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-04-15 12:54:13 +0300

    change routing logic to accomodate about page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import About from './components/About';
 import Home from './components/Home';
 import Choose from './components/ChooseSeats';
 import Tickets from './components/Tickets';
@@ -7,14 +8,18 @@ import Tickets from './components/Tickets';
 const AppRouter = (parentProps) => {
   // const parentProps = props;
   return (
-    <Switch>
-      <Route exact path='/' render={(props) => <Home {...props} {...parentProps} />} />
-      <Route path='/home' render={(props) => <Home {...props} {...parentProps} />} />
-      {/* <Route path='/home' component={Home} /> */}
-      <Route path='/choose' render={(props) => <Choose {...props} {...parentProps} />} />
-      <Route path='/tickets' render={(props) => <Tickets {...props} {...parentProps} />} />
-      <Redirect to='/home' />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path='/' render={(props) => <Redirect to='/about' />} />
+        <Route path='/about' render={(props) => <About {...props} {...parentProps} />} />
+        <Route path='/home' render={(props) => <Home {...props} {...parentProps} />} />
+        {/* <Route path='/home' component={Home} /> */}
+        <Route path='/choose' render={(props) => <Choose {...props} {...parentProps} />} />
+        <Route path='/tickets' render={(props) => <Tickets {...props} {...parentProps} />} />
+        <Redirect to='/about' />
+      </Switch>
+    </>
+
   )
 };
 
