9bd4a0fe2f118fcf715edfa23412d9d43fd75dc0
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2019-04-15 12:56:19 +0300

    add navigation and fix onlcick of movie title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Navigation from './Navigation';
 import IMDBRating from './IMDBRating';
 import YouTubeLink from './YouTubeLink';
 
@@ -9,11 +10,12 @@ const Home = (props) => {
   }
   return (
     <>
+      <Navigation />
       <h2>Now showing:</h2>
-      <div className="movies-container">
+      <div className='movies-container'>
         {props.movies.map((movie, i) => (
           <div key={i} className='pick-movie-container'>
-            <div className='movie-title' onClick={() => {props.setStateVal('selectedMovie', movie.imdbId)}}>{movie.title}</div>
+            <div className='movie-title' onClick={() => { props.setStateVal('selectedMovie', movie.imdbId) }}>{movie.title}</div>
             <div className='movie-details'>
               <IMDBRating titleId={movie.imdbId} />
               <YouTubeLink videoId={movie.trailerYouTubeId} />
